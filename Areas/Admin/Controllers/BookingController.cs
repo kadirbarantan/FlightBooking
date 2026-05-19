1c32816e145001d6e416c85d46bc53c238573849
kadirbarantan <tankadirbaran0@gmail.com>
2026-05-19 19:36:25 +0300

    Create Booking & Passenger Sayfası ve DTO tasarımı

---
+++ Areas/Admin/Controllers/BookingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    public class BookingController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+    }
+}
